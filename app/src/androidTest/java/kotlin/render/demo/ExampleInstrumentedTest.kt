7fe60b5de65c0ec8c94bb0b1454cf08237e4e4c4
gayankuruppu <example@example.com>
2019-09-19 11:39:04 +0530

    Update Gradle

--- app/src/androidTest/java/kotlin/render/demo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/kotlin/render/demo/ExampleInstrumentedTest.kt
@@ -1,7 +1,8 @@
 package kotlin.render.demo
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+
 
 import org.junit.Test
 import org.junit.runner.RunWith
