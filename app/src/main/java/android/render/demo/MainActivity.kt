edc347103d26cbe59b015e61976c621cd46b43c3
gayankuruppu <example@example.com>
2019-09-19 11:25:11 +0530

    Rename App

---
+++ app/src/main/java/android/render/demo/MainActivity.kt
@@ -0,0 +1,22 @@
+package android.render.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import render.animations.*
+import kotlin.render.demo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.TextView)
+
+        val render = Render(this)
+        render.setAnimation(Attention().bounce(textView))
+        render.setDuration(2000)
+        render.start()
+    }
+}
